738a8ee6a5a133461b7d288ebcee78cabe4dc901
Aparajita Fishman <aparajita@aparajita.com>
2015-10-02 11:56:30 -0700

    Minimum node is now 0.12, added travis config

--- test/generate.js
+++ test/generate.js
@@ -1,9 +1,9 @@
 "use strict";
 
-let child_process = require("child_process"),
+var child_process = require("child_process"),
     fs = require("fs");
 
-let output = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
+var output = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
 
 if (output.status === 1)
 {
@@ -13,4 +13,4 @@ if (output.status === 1)
 else if (output.status === 0)
     console.log("eslint returned no errors!");
 else
-    console.log(`eslint returned status ${output.status}: ${output.stderr}`);
+    console.log("eslint returned status %d: %s", output.status, output.stderr);
--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 "use strict";
 
-let child_process = require("child_process"),
+var child_process = require("child_process"),
     fs = require("fs");
 
-let output = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
+var output = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
 
 if (output.status === 1)
 {
-    let fixture = fs.readFileSync("test/fixtures/errors.txt", "utf8");
+    var fixture = fs.readFileSync("test/fixtures/errors.txt", "utf8");
 
     if (fixture === output.stdout)
         process.exit(0);
