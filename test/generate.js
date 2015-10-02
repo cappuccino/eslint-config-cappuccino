eca02365614ec4b8ab0036f7300cc10d314f98f7
Aparajita Fishman <aparajita@aparajita.com>
2015-10-02 13:28:01 -0700

    Test strips path so it's host-agnostic

--- test/generate.js
+++ test/generate.js
@@ -3,14 +3,18 @@
 var child_process = require("child_process"),
     fs = require("fs");
 
-var output = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
+var result = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f stylish --no-color test/fixtures".split(" "), { encoding: "utf8" });
 
-if (output.status === 1)
+if (result.status === 1)
 {
-    fs.writeFileSync("test/fixtures/errors.txt", output.stdout);
-    console.log("fixtures generated");
+    // Strip file path
+    var output = result.stdout.trim().split("\n");
+
+    output.shift();
+    fs.writeFileSync("test/fixtures/errors.txt", output.join("\n"));
+    console.log("Fixtures generated");
 }
-else if (output.status === 0)
+else if (result.status === 0)
     console.log("eslint returned no errors!");
 else
-    console.log("eslint returned status %d: %s", output.status, output.stderr);
+    console.log("eslint returned status %d: %s", result.status, result.stderr);
--- test/test.js
+++ test/test.js
@@ -3,13 +3,19 @@
 var child_process = require("child_process"),
     fs = require("fs");
 
-var result = child_process.spawnSync("node_modules/.bin/eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
+var result = child_process.spawnSync("./node_modules/.bin/eslint", "--no-eslintrc -c ./lib/eslint.json --rulesdir ./lib/rules -f stylish --no-color ./test/fixtures".split(" "), { encoding: "utf8" });
 
 if (result.status === 1)
 {
-    var fixture = fs.readFileSync("test/fixtures/errors.txt", { encoding: "utf8" });
+    // Strip file path
+    var output = result.stdout.trim().split("\n");
 
-    if (fixture === result.stdout)
+    output.shift();
+    output = output.join("\n");
+
+    var fixture = fs.readFileSync("test/fixtures/errors.txt", "utf8");
+
+    if (fixture === output)
     {
         console.log("Test passed");
         process.exit(0);
