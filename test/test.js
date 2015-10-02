ce317f6a39e18c3bfcfbfdaff31b957e10a540b6
Aparajita Fishman <aparajita@aparajita.com>
2015-10-02 12:30:07 -0700

    Fixed test, show a message

--- test/test.js
+++ test/test.js
@@ -3,14 +3,18 @@
 var child_process = require("child_process"),
     fs = require("fs");
 
-var output = child_process.spawnSync("eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
+var result = child_process.spawnSync("node_modules/.bin/eslint", "-c lib/eslint.json --rulesdir lib/rules -f compact test/fixtures".split(" "), { encoding: "utf8" });
 
-if (output.status === 1)
+if (result.status === 1)
 {
     var fixture = fs.readFileSync("test/fixtures/errors.txt", "utf8");
 
-    if (fixture === output.stdout)
+    if (fixture === result.stdout)
+    {
+        console.log("test passed");
         process.exit(0);
+    }
 }
 
+console.log("test failed with status ", result.status);
 process.exit(1);
