043fa36e1201ffe2409fb1f8eccde8d0bd9d099b
Aparajita Fishman <aparajita@aparajita.com>
2015-10-07 10:21:40 -0400

    Refined some rules, cleaned up test fixture

--- test/fixtures/test.js
+++ test/fixtures/test.js
@@ -10,6 +10,8 @@ let array = [1, 2,];
 
 // no-cond-assign
 // no-constant-condition
+let x = 13;
+
 if (x = 7)
     console.log(x); // no-console: don't warn
 
@@ -250,8 +252,8 @@ function noElseReturn()
 label:
     foo = 7;
 
-// no-eq-null: covered by eqeqeq
-if (n == null)
+// no-eq-null: don't warn
+if (x == null)
     foo();
 
 // no-eval
@@ -261,30 +263,30 @@ let evil = eval("7");
 Object.prototype.testMe = 7;
 
 // no-extra-bind
-let x = function ()
-{   /*error The function binding is unnecessary.*/
+x = function ()
+{   /* error The function binding is unnecessary.*/
     foo();
 }.bind(bar);
 
 let bindme = (() =>
-{        /*error The function binding is unnecessary.*/
+{        /* error The function binding is unnecessary.*/
     foo();
 }).bind(bar);
 
 bindme = (() =>
-{        /*error The function binding is unnecessary.*/
+{        /* error The function binding is unnecessary.*/
     this.foo();
 }).bind(bar);
 
 bindme = function ()
-{   /*error The function binding is unnecessary.*/
+{   /* error The function binding is unnecessary.*/
     (function () {
         this.foo();
     })();
 }.bind(bar);
 
 bindme = function ()
-{   /*error The function binding is unnecessary.*/
+{   /* error The function binding is unnecessary.*/
     function foobar() {
         this.bar();
     }
@@ -314,61 +316,67 @@ setTimeout("alert('Hi!');", 100);
 // no-invalid-this
 function invalidThis()
 {
-    "use strict";
+    this.a = 0;            /* error Unexpected `this`.*/
+    baz(() => this);       /* error Unexpected `this`.*/
 
-    this.a = 0;            /*error Unexpected `this`.*/
-    baz(() => this);       /*error Unexpected `this`.*/
-
-    (function() {
-        this.a = 0;        /*error Unexpected `this`.*/
-        baz(() => this);   /*error Unexpected `this`.*/
+    (function()
+    {
+        this.a = 0;        /* error Unexpected `this`.*/
+        baz(() => this);   /* error Unexpected `this`.*/
     })();
 
     function foo() {
-        this.a = 0;        /*error Unexpected `this`.*/
-        baz(() => this);   /*error Unexpected `this`.*/
+        this.a = 0;        /* error Unexpected `this`.*/
+        baz(() => this);   /* error Unexpected `this`.*/
     }
 
-    let foo = function() {
-        this.a = 0;        /*error Unexpected `this`.*/
-        baz(() => this);   /*error Unexpected `this`.*/
+    let foo = function()
+    {
+        this.a = 0;        /* error Unexpected `this`.*/
+        baz(() => this);   /* error Unexpected `this`.*/
     };
 
-    foo(function() {
-        this.a = 0;        /*error Unexpected `this`.*/
-        baz(() => this);   /*error Unexpected `this`.*/
+    foo(function()
+    {
+        this.a = 0;        /* error Unexpected `this`.*/
+        baz(() => this);   /* error Unexpected `this`.*/
     });
 
-    obj.foo = () => {
+    x.foo = () =>
+    {
         // `this` of arrow functions is the outer scope's.
-        this.a = 0;        /*error Unexpected `this`.*/
+        this.a = 0;        /* error Unexpected `this`.*/
     };
 
     let obj = {
-        aaa() {
-            return () => {
+        aaa()
+        {
+            return () =>
+            {
                 // There is in a method `aaa`, but `foo` is not a method.
-                this.a = 0;      /*error Unexpected `this`.*/
-                baz(() => this); /*error Unexpected `this`.*/
+                this.a = 0;      /* error Unexpected `this`.*/
+                baz(() => this); /* error Unexpected `this`.*/
             };
         }
     };
 
     class Foo {
-        static foo() {
-            this.a = 0;      /*error Unexpected `this`.*/
-            baz(() => this); /*error Unexpected `this`.*/
+        static foo()
+        {
+            this.a = 0;      /* error Unexpected `this`.*/
+            baz(() => this); /* error Unexpected `this`.*/
         }
     }
 
-    foo.forEach(function() {
-        this.a = 0;          /*error Unexpected `this`.*/
-        baz(() => this);     /*error Unexpected `this`.*/
+    foo.forEach(function()
+    {
+        this.a = 0;          /* error Unexpected `this`.*/
+        baz(() => this);     /* error Unexpected `this`.*/
     });
 }
 
 // no-iterator
-foo.__iterator__ = function () {}; // error Reserved name '__iterator__'
+foo.__iterator__ = function() {}; // error Reserved name '__iterator__'
 
 // no-lone-blocks
 {
@@ -407,7 +415,6 @@ let s = "line 1\
 
 // no-native-reassign
 String = 7;
-undefined = 13;
 
 // no-new-func
 let f1 = new Function("a", "b", "return a + b"),
@@ -478,20 +485,20 @@ while (val = foo(), val < 42);
 function noThrowLiteral()
 {
     /* eslint-disable no-unreachable */
-    throw "error";         /*error Expected an object to be thrown.*/
+    throw "error";         /* error Expected an object to be thrown.*/
 
-    throw 0;               /*error Expected an object to be thrown.*/
+    throw 0;               /* error Expected an object to be thrown.*/
 
-    throw undefined;       /*error Do not throw undefined.*/
+    throw undefined;       /* error Do not throw undefined.*/
 
-    throw null;            /*error Expected an object to be thrown.*/
+    throw null;            /* error Expected an object to be thrown.*/
 
     var err = new Error();
-    throw "an " + err;     /*error Expected an object to be thrown.*/
+    throw "an " + err;     /* error Expected an object to be thrown.*/
     // err is recast to a string literal
 
     var err = new Error();
-    throw `${err}`;         /*error Expected an object to be thrown.*/
+    throw `${err}`;         /* error Expected an object to be thrown.*/
 
     /* eslint-enable */
 }
@@ -501,22 +508,22 @@ function noThrowLiteral()
 
 // no-useless-call
 // These are same as `foo(1, 2, 3);`
-foo.call(undefined, 1, 2, 3);     /*error unnecessary ".call()".*/
-foo.apply(undefined, [1, 2, 3]);  /*error unnecessary ".apply()".*/
-foo.call(null, 1, 2, 3);          /*error unnecessary ".call()".*/
-foo.apply(null, [1, 2, 3]);       /*error unnecessary ".apply()".*/
+foo.call(undefined, 1, 2, 3);     /* error unnecessary ".call()".*/
+foo.apply(undefined, [1, 2, 3]);  /* error unnecessary ".apply()".*/
+foo.call(null, 1, 2, 3);          /* error unnecessary ".call()".*/
+foo.apply(null, [1, 2, 3]);       /* error unnecessary ".apply()".*/
 
 // These are same as `obj.foo(1, 2, 3);`
-obj.foo.call(obj, 1, 2, 3);       /*error unnecessary ".call()".*/
-obj.foo.apply(obj, [1, 2, 3]);    /*error unnecessary ".apply()".*/
+obj.foo.call(obj, 1, 2, 3);       /* error unnecessary ".call()".*/
+obj.foo.apply(obj, [1, 2, 3]);    /* error unnecessary ".apply()".*/
 
 // no-useless-concat
 // these are the same as "10"
-let a = `some` + `string`; /*error Unexpected string concatenation of literals.*/
-let a = '1' + '0';         /*error Unexpected string concatenation of literals.*/
-let a = '1' + `0`;         /*error Unexpected string concatenation of literals.*/
-let a = `1` + '0';         /*error Unexpected string concatenation of literals.*/
-let a = `1` + `0`;         /*error Unexpected string concatenation of literals.*/
+let a = `some` + `string`; /* error Unexpected string concatenation of literals.*/
+let a = '1' + '0';         /* error Unexpected string concatenation of literals.*/
+let a = '1' + `0`;         /* error Unexpected string concatenation of literals.*/
+let a = `1` + '0';         /* error Unexpected string concatenation of literals.*/
+let a = `1` + `0`;         /* error Unexpected string concatenation of literals.*/
 
 // no-warning-comments
 // TODO: Finish this file
@@ -526,29 +533,29 @@ let a = `1` + `0`;         /*error Unexpected string concatenation of literals.*
 with (foo) { doSomething(); }
 
 // radix: don't warn
-let x = parseInt("27");
+x = parseInt("27");
 
 // wrap-iife
 let wrap = (function () { return { y: 1 };}());
 
 // yoda
-if ("red" === color) {          /*error Expected literal to be on the right side of ===.*/
+if ("red" === color) {          /* error Expected literal to be on the right side of ===.*/
     // ...
 }
 
-if (true === flag) {             /*error Expected literal to be on the right side of ==.*/
+if (true === flag) {             /* error Expected literal to be on the right side of ==.*/
     // ...
 }
 
-if (5 > count) {                /*error Expected literal to be on the right side of >.*/
+if (5 > count) {                /* error Expected literal to be on the right side of >.*/
     // ...
 }
 
-if (-1 < str.indexOf(substr)) { /*error Expected literal to be on the right side of <.*/
+if (-1 < x.indexOf(substr)) { /* error Expected literal to be on the right side of <.*/
     // ...
 }
 
-if (0 <= x && x < 1) {          /*error Expected literal to be on the right side of <=.*/
+if (0 <= x && x < 1) {          /* error Expected literal to be on the right side of <=.*/
     // ...
 }
 
@@ -608,16 +615,16 @@ function notUsed(arg) {}
 /* eslint-disable no-unused-vars */
 
 // no-use-before-define
-f();            /*error f was used before it was defined*/
+f();            /* error f was used before it was defined*/
 function f() {}
 
 function g() {
-    return b;  /*error b was used before it was defined*/
+    return b;  /* error b was used before it was defined*/
 }
 let b = 1;
 
 {
-    doSomething(c);  /*error c was used before it was defined*/
+    doSomething(c);  /* error c was used before it was defined*/
     let c = 1;
 }
 
@@ -628,7 +635,7 @@ let b = 1;
 // callback-return
 function foo() {
     if (err) {
-        callback(err); /*error Expected return with your callback function.*/
+        callback(err); /* error Expected return with your callback function.*/
     }
     callback();
 }
@@ -653,8 +660,8 @@ let a = [ 1, 2];
 a = [1, 2 ];
 
 // block-spacing
-function foo() {return true;} /*error Requires a space after "{".*/ /*error Requires a space before "}".*/
-if (foo) { foo = 0;}          /*error Requires a space before "}".*/
+function foo() {return true;} /* error Requires a space after "{".*/ /* error Requires a space before "}".*/
+if (foo) { foo = 0;}          /* error Requires a space before "}".*/
 
 // brace-style
 if (foo) {
@@ -674,22 +681,22 @@ else {
 }
 
 // comma-spacing
-let foo = 1 ,bar = 2;                   /*error There should be no space before ','.*/ /*error A space is required after ','.*/
-let arr = [1 , 2];                      /*error There should be no space before ','.*/
-let obj = { "foo": "bar" ,"baz": "qur" }; /*error There should be no space before ','.*/ /*error A space is required after ','.*/
-foo(a ,b);                              /*error There should be no space before ','.*/ /*error A space is required after ','.*/
-let e = new Error(a ,b);                          /*error There should be no space before ','.*/ /*error A space is required after ','.*/
+let foo = 1 ,bar = 2;                   /* error There should be no space before ','.*/ /* error A space is required after ','.*/
+let arr = [1 , 2];                      /* error There should be no space before ','.*/
+let obj = { "foo": "bar" ,"baz": "qur" }; /* error There should be no space before ','.*/ /* error A space is required after ','.*/
+foo(a ,b);                              /* error There should be no space before ','.*/ /* error A space is required after ','.*/
+let e = new Error(a ,b);                          /* error There should be no space before ','.*/ /* error A space is required after ','.*/
 function foob(ab ,ba)
 {
-}                    /*error There should be no space before ','.*/ /*error A space is required after ','.*/
+}                    /* error There should be no space before ','.*/ /* error A space is required after ','.*/
 
 // comma-style
 let first = 7
   , second = 13;
 
 // computed-property-spacing
-foo = object[ foo];
-foo = object[foo ];
+foo = x[ foo];
+foo = x[foo ];
 
 // key-spacing
 let key = { k :7 };
@@ -716,6 +723,7 @@ function one()
 let n = new foo();
 
 // new-parens
+let Foo = {};
 let np = new Foo;
 
 // newline-after-var
@@ -922,7 +930,7 @@ class A extends B
 {
     constructor()
     {
-        this.a = 0;        /*error "this" is not allowed before super()*/
+        this.a = 0;        /* error "this" is not allowed before super()*/
         super();
     }
 }
@@ -931,7 +939,7 @@ class A extends B
 {
     constructor()
     {
-        this.foo();        /*error "this" is not allowed before super()*/
+        this.foo();        /* error "this" is not allowed before super()*/
         super();
     }
 }
@@ -940,7 +948,7 @@ class A extends B
 {
     constructor()
     {
-        super.foo();       /*error "super" is not allowed before super()*/
+        super.foo();       /* error "super" is not allowed before super()*/
         super();
     }
 }
@@ -949,7 +957,7 @@ class A extends B
 {
     constructor()
     {
-        super(this.foo()); /*error "this" is not allowed before super()*/
+        super(this.foo()); /* error "this" is not allowed before super()*/
     }
 }
 
